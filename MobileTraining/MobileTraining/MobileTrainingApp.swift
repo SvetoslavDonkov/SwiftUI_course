72f420f59bdc7f0375876cf4adb3b30e6c739970
Svetoslav Donkov <svetoslavdonkov@gmail.com>
2023-05-08 17:37:21 +0300

    Backbone

---
+++ MobileTraining/MobileTraining/MobileTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileTrainingApp.swift
+//  MobileTraining
+//
+//  Created by Svetoslav Donkov on 8.05.23.
+//
+
+import SwiftUI
+
+@main
+struct MobileTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
